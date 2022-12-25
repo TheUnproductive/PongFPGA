8061e831b6641f46b0480757fd4f9c9459510116
justu <geil.js18@gmail.com>
2022-12-25 18:17:14 +0100

    Update testbench

--- testbench/pong_tb.vhd
+++ testbench/pong_tb.vhd
@@ -9,8 +9,12 @@ architecture testbench of pong_tb is
 
     signal clk : std_ulogic := '0';
     signal hsync_tb : std_ulogic;
-    signal vsync_tb : std_ulogic;
     signal hreset_tb : std_ulogic;
+    signal vsync_tb : std_ulogic;
+
+    signal hsync_count : std_ulogic_vector(8 downto 0);
+    signal hblanking : std_ulogic;
+    signal hblanking_n : std_ulogic;
 
     component pong is
         port(
@@ -24,15 +28,24 @@ architecture testbench of pong_tb is
         port(
             clk : in std_ulogic;
             hsync_out : out std_ulogic;
-            hreset_out : out std_ulogic
+            hreset_out : out std_ulogic;
+            hreset_n_out : out std_ulogic;
+            hblanking : out std_ulogic;
+            hblanking_n : out std_ulogic;
+            hsync_count : out std_ulogic_vector(8 downto 0)
         );
     end component;
 
     component vsync is
-        port(
-            clk : in std_ulogic;
-            vsync_out : out std_ulogic
-        );
+    Port(
+        clk : in std_ulogic;
+        vsync_out : out std_ulogic;
+        vreset_out : out std_ulogic;
+        vreset_n_out : out std_ulogic;
+        vblanking : out std_ulogic;
+        vblanking_n : out std_ulogic;
+        vsync_count : out std_ulogic_vector(8 downto 0)
+    );
     end component;
 
 begin
@@ -43,15 +56,26 @@ begin
     --     vsync_out => vsync_tb
     -- );
 
-    hsync_test : hsync port map(
+    hsync_test : hsync 
+    port map(
         clk => clk,
         hsync_out => hsync_tb,
-        hreset_out => hreset_tb
+        hreset_out => hreset_tb,
+        hreset_n_out => open,
+        hblanking => hblanking,
+        hblanking_n => hblanking_n,
+        hsync_count => hsync_count
     );
 
-    vsync_test : vsync port map(
-        clk => clk,
-        vsync_out => vsync_tb
+    vsync_test : vsync 
+    port map(
+        clk => hreset_tb,
+        vsync_out => vsync_tb,
+        vreset_out => open,
+        vreset_n_out => open,
+        vblanking => open,
+        vblanking_n => open,
+        vsync_count => open
     );
 
     clk_process : process
